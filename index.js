9122fa14aeaf6803025aeabfe8ba7d373a7cca61
Joshua Wu <Joshua.wu.0910@gmail.com>
2022-11-07 17:25:35 -0800

    remove twitter

--- index.js
+++ index.js
@@ -11,7 +11,6 @@ ${chalk.bold('Github:')} https://github.com/jwu910
 ${chalk.bold('LinkedIn:')} https://linkedin.com/in/wujoshua
 ${chalk.bold('NPM:')} https://www.npmjs.com/~jwu910
 ${chalk.bold('Portfolio:')} http://joshuawu.me
-${chalk.bold('Twitter:')} https://twitter.com/jwu910
 `,
     {
         borderColor: 'cyan',
