e9870eba67174f760580ba6b64cd1ed6663dd6e6
Joshua Wu <code@joshuawu.me>
2024-07-17 23:51:35 -0700

    chore: comment out mentorship calls

--- index.js
+++ index.js
@@ -11,8 +11,8 @@ ${chalk.bold('Github:')} https://github.com/jwu910
 ${chalk.bold('LinkedIn:')} https://linkedin.com/in/wujoshua
 ${chalk.bold('NPM:')} https://www.npmjs.com/~jwu910
 ${chalk.bold('Portfolio:')} https://joshuawu.me
-${chalk.bold('Schedule a Mentorship Call:')} https://adplist.org/mentors/joshua-wu?session=28642-11-with-josh-wu
 `,
+// ${chalk.bold('Schedule a Mentorship Call:')} https://adplist.org/mentors/joshua-wu?session=28642-11-with-josh-wu
     {
         borderColor: 'cyan',
         borderStyle: 'double',
